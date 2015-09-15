f44109ff8bc0a8b6a41a66f47f1a9e451d57a456
caguthrie <christopher.a.guthrie@gmail.com>
2015-09-15 10:41:27 -0400

    Unable to click dropdown in some situations

--- typeahead-focus.js
+++ typeahead-focus.js
@@ -10,15 +10,33 @@
  */
 
 angular.module('typeahead-focus', [])
-    .directive('typeaheadFocus', function () {
+    .directive('typeaheadFocus', function ($timeout) {
       return {
         require: 'ngModel',
         link: function (scope, element, attr, ngModel) {
+
+          // Have we already overriden angular ui bootstrap typeahead parser?
+          var setParser = false;
 
           // Array of keyCode values for arrow keys and enter
           const ARROW_KEYS = [13,37,38,39,40];
 
           function manipulateViewValue(e) {
+            /*
+             * This is a workaround to not call the angular-ui bootstrap parser in the typeahead directive
+             * if the input is blank.  Because the typeahead was not designed for that, strange activities
+             * occur such as being unable to click an element in the typeahead if input already existed.
+             */
+            if( !setParser ){
+              const oldFunc = ngModel.$parsers[0];
+              ngModel.$parsers[0] = function(inputValue){
+                if( inputValue == "")
+                  return;
+                oldFunc(inputValue);
+              };
+              setParser = true;
+            }
+
             /* we have to check to see if the arrow keys were in the input because if they were trying to select
              * a menu option in the typeahead, this may cause unexpected behavior if we were to execute the rest
              * of this function
